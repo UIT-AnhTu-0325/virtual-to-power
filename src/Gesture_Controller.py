681cd2d705e8623d9fbea0b7edf65d2b02dc4dc1
tu.dang <19520325@gm.uit.edu.vn>
2023-02-01 00:58:17 +0700

    Init function

---
+++ src/Gesture_Controller.py
@@ -0,0 +1,125 @@
+# Imports
+
+import cv2
+import mediapipe as mp
+import pyautogui
+import math
+from enum import IntEnum
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from google.protobuf.json_format import MessageToDict
+import screen_brightness_control as sbcontrol
+
+pyautogui.FAILSAFE = False
+mp_drawing = mp.solutions.drawing_utils
+mp_hands = mp.solutions.hands
+
+# Gesture Encodings 
+class Gest(IntEnum):
+    # Binary Encoded
+    """
+    Enum for mapping all hand gesture to binary number.
+    """
+
+    FIST = 0
+    PINKY = 1
+    RING = 2
+    MID = 4
+    LAST3 = 7
+    INDEX = 8
+    FIRST2 = 12
+    LAST4 = 15
+    THUMB = 16    
+    PALM = 31
+    
+    # Extra Mappings
+    V_GEST = 33
+    TWO_FINGER_CLOSED = 34
+    PINCH_MAJOR = 35
+    PINCH_MINOR = 36
+
+# Multi-handedness Labels
+class HLabel(IntEnum):
+    MINOR = 0
+    MAJOR = 1
+
+# Convert Mediapipe Landmarks to recognizable Gestures
+class HandRecog:
+    """
+    Convert Mediapipe Landmarks to recognizable Gestures.
+    """
+    
+    def __init__(self, hand_label):
+        """
+        Constructs all the necessary attributes for the HandRecog object.
+
+        Parameters
+        ----------
+            finger : int
+                Represent gesture corresponding to Enum 'Gest',
+                stores computed gesture for current frame.
+            ori_gesture : int
+                Represent gesture corresponding to Enum 'Gest',
+                stores gesture being used.
+            prev_gesture : int
+                Represent gesture corresponding to Enum 'Gest',
+                stores gesture computed for previous frame.
+            frame_count : int
+                total no. of frames since 'ori_gesture' is updated.
+            hand_result : Object
+                Landmarks obtained from mediapipe.
+            hand_label : int
+                Represents multi-handedness corresponding to Enum 'HLabel'.
+        """
+
+        self.finger = 0
+        self.ori_gesture = Gest.PALM
+        self.prev_gesture = Gest.PALM
+        self.frame_count = 0
+        self.hand_result = None
+        self.hand_label = hand_label
+    
+    def update_hand_result(self, hand_result):
+        self.hand_result = hand_result
+
+    def get_signed_dist(self, point):
+        """
+        returns signed euclidean distance between 'point'.
+
+        Parameters
+        ----------
+        point : list contaning two elements of type list/tuple which represents 
+            landmark point.
+        
+        Returns
+        -------
+        float
+        """
+        sign = -1
+        if self.hand_result.landmark[point[0]].y < self.hand_result.landmark[point[1]].y:
+            sign = 1
+        dist = (self.hand_result.landmark[point[0]].x - self.hand_result.landmark[point[1]].x)**2
+        dist += (self.hand_result.landmark[point[0]].y - self.hand_result.landmark[point[1]].y)**2
+        dist = math.sqrt(dist)
+        return dist*sign
+    
+    def get_dist(self, point):
+        """
+        returns euclidean distance between 'point'.
+
+        Parameters
+        ----------
+        point : list contaning two elements of type list/tuple which represents 
+            landmark point.
+        
+        Returns
+        -------
+        float
+        """
+        dist = (self.hand_result.landmark[point[0]].x - self.hand_result.landmark[point[1]].x)**2
+        dist += (self.hand_result.landmark[point[0]].y - self.hand_result.landmark[point[1]].y)**2
+        dist = math.sqrt(dist)
+        return dist
+    
+    
