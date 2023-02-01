dc21cc9c4f54f3debcf9edf6ee1114f9d2af8df6
tu.dang <19520325@gm.uit.edu.vn>
2023-02-01 17:52:44 +0700

    Enum handler animation

---
+++ src/Gest.py
@@ -0,0 +1,25 @@
+from enum import IntEnum
+
+# Gesture Encodings 
+class Gest(IntEnum):
+    # Binary Encoded
+    """
+    Enum for mapping all hand gesture to binary number.
+    """
+
+    FIST = 0
+    PINKY = 1
+    RING = 2
+    MID = 4
+    LAST3 = 7
+    INDEX = 8
+    FIRST2 = 12
+    LAST4 = 15
+    THUMB = 16    
+    PALM = 31
+    
+    # Extra Mappings
+    V_GEST = 33
+    TWO_FINGER_CLOSED = 34
+    PINCH_MAJOR = 35
+    PINCH_MINOR = 36
--- src/Virtual-mouse-controller.py
+++ src/Virtual-mouse-controller.py
@@ -11,34 +11,11 @@ from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
 from google.protobuf.json_format import MessageToDict
 import screen_brightness_control as sbcontrol
 from HLabel import HLabel
+from Gest import Gest
 
 pyautogui.FAILSAFE = False
 mp_drawing = mp.solutions.drawing_utils
 mp_hands = mp.solutions.hands
-
-# Gesture Encodings 
-class Gest(IntEnum):
-    # Binary Encoded
-    """
-    Enum for mapping all hand gesture to binary number.
-    """
-
-    FIST = 0
-    PINKY = 1
-    RING = 2
-    MID = 4
-    LAST3 = 7
-    INDEX = 8
-    FIRST2 = 12
-    LAST4 = 15
-    THUMB = 16    
-    PALM = 31
-    
-    # Extra Mappings
-    V_GEST = 33
-    TWO_FINGER_CLOSED = 34
-    PINCH_MAJOR = 35
-    PINCH_MINOR = 36
 
 # Convert Mediapipe Landmarks to recognizable Gestures
 class HandRecog:
